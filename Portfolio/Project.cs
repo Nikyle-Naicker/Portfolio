6dbed07add9076fb0268f57309055d341e455ca5
Nikyle-Naicker <nikylen@gmail.com>
2025-02-28 16:26:17 +0200

    updated the project service to store and display project images dynamically, updated the home page and project page to display more information to the user.

--- Portfolio/Project.cs
+++ Portfolio/Project.cs
@@ -2,20 +2,23 @@
 {
     public class Project
     {
-        public Project(string title, string description, string note, string github, string liveLink)
+        public Project(string title, string description, string projectThumbnail ,string note, string github, string liveLink, int projectImageId)
         {
             Title = title;
             Description = description;
+            ProjectThumbnail = projectThumbnail;
             Note = note;
             Github = github;
             Live_Link = liveLink;
+            ProjectImageId = projectImageId;
         }
         public string Title { get; set; }
         public string Description { get; set; }
+        public string ProjectThumbnail { get; set; }
         public string Note { get; set; }
         public string Github { get; set; }
         public string Live_Link { get; set; }
-
+        public int ProjectImageId { get; set; }
       
     }
 }
--- Portfolio/ProjectService.cs
+++ Portfolio/ProjectService.cs
@@ -5,6 +5,7 @@ namespace Portfolio
     public class ProjectService : IProjectService
     {
         private readonly IList<Project> _projects;
+        private readonly IList<ProjectImages> _images;
 
         public ProjectService() 
         {
@@ -12,144 +13,43 @@ namespace Portfolio
             {
                  new Project(
                     "Finshark",
-                    "A stock app",
-                    "Server startup takes long",
+                    "A stock app that allows users to register an account, search for, and view various stock options from Nasdaq(provided by FMP)," +
+                    " the user can add the stock options to their portfolio and can comment on the selected stock.",
+                    "/pictures/finshark/finsharkhome.png",
+                    "The server in use is on a free tier of a hosting service, so expect delays when using the app at first",
                     "https://github.com/Nikyle-Naicker/API",
-                    "https://delightful-island-0a0d63a1e.4.azurestaticapps.net/"),
+                    "https://delightful-island-0a0d63a1e.4.azurestaticapps.net/",
+                    1),
+                    
                  new Project(
                      "Dewey",
                      "Library game",
-                     "Third game not implemented yet",
+                     "/pictures/dewey/deweygamesort.png",
+                     "",
                      "https://github.com/Nikyle-Naicker/Library_Game",
-                     "N/A"),
-                 new Project(
-                     "Dewey",
-                     "Library game",
-                     "Third game not implemented yet",
-                     "https://github.com/Nikyle-Naicker/Library_Game",
-                     "N/A"),
-                 new Project(
-                     "Dewey",
-                     "Library game",
-                     "Third game not implemented yet",
-                     "https://github.com/Nikyle-Naicker/Library_Game",
-                     "N/A"),
-                 new Project(
-                     "Dewey",
-                     "Library game",
-                     "Third game not implemented yet",
-                     "https://github.com/Nikyle-Naicker/Library_Game",
-                     "N/A"),
-                 new Project(
-                     "Dewey",
-                     "Library game",
-                     "Third game not implemented yet",
-                     "https://github.com/Nikyle-Naicker/Library_Game",
-                     "N/A"),
-                 new Project(
-                     "Dewey",
-                     "Library game",
-                     "Third game not implemented yet",
-                     "https://github.com/Nikyle-Naicker/Library_Game",
-                     "N/A"),
-                 new Project(
-                     "Dewey",
-                     "Library game",
-                     "Third game not implemented yet",
-                     "https://github.com/Nikyle-Naicker/Library_Game",
-                     "N/A"),
-                 new Project(
-                     "Dewey",
-                     "Library game",
-                     "Third game not implemented yet",
-                     "https://github.com/Nikyle-Naicker/Library_Game",
-                     "N/A"),
-                 new Project(
-                     "Dewey",
-                     "Library game",
-                     "Third game not implemented yet",
-                     "https://github.com/Nikyle-Naicker/Library_Game",
-                     "N/A"),
-                 new Project(
-                     "Dewey",
-                     "Library game",
-                     "Third game not implemented yet",
-                     "https://github.com/Nikyle-Naicker/Library_Game",
-                     "N/A"),
-                 new Project(
-                     "Dewey",
-                     "Library game",
-                     "Third game not implemented yet",
-                     "https://github.com/Nikyle-Naicker/Library_Game",
-                     "N/A"),
-                 new Project(
-                     "Dewey",
-                     "Library game",
-                     "Third game not implemented yet",
-                     "https://github.com/Nikyle-Naicker/Library_Game",
-                     "N/A"),
-                 new Project(
-                     "Dewey",
-                     "Library game",
-                     "Third game not implemented yet",
-                     "https://github.com/Nikyle-Naicker/Library_Game",
-                     "N/A"),
-                 new Project(
-                     "Dewey",
-                     "Library game",
-                     "Third game not implemented yet",
-                     "https://github.com/Nikyle-Naicker/Library_Game",
-                     "N/A"),
-                 new Project(
-                     "Dewey",
-                     "Library game",
-                     "Third game not implemented yet",
-                     "https://github.com/Nikyle-Naicker/Library_Game",
-                     "N/A"),
-                 new Project(
-                     "Dewey",
-                     "Library game",
-                     "Third game not implemented yet",
-                     "https://github.com/Nikyle-Naicker/Library_Game",
-                     "N/A"),
-                 new Project(
-                     "Dewey",
-                     "Library game",
-                     "Third game not implemented yet",
-                     "https://github.com/Nikyle-Naicker/Library_Game",
-                     "N/A"),
-                 new Project(
-                     "Dewey",
-                     "Library game",
-                     "Third game not implemented yet",
-                     "https://github.com/Nikyle-Naicker/Library_Game",
-                     "N/A"),
-                 new Project(
-                     "Dewey",
-                     "Library game",
-                     "Third game not implemented yet",
-                     "https://github.com/Nikyle-Naicker/Library_Game",
-                     "N/A"),
-                 new Project(
-                     "Dewey",
-                     "Library game",
-                     "Third game not implemented yet",
-                     "https://github.com/Nikyle-Naicker/Library_Game",
-                     "N/A"),
-                 new Project(
-                     "Dewey",
-                     "Library game",
-                     "Third game not implemented yet",
-                     "https://github.com/Nikyle-Naicker/Library_Game",
-                     "N/A"),
-                 new Project(
-                     "Dewey",
-                     "Library game",
-                     "Third game not implemented yet",
-                     "https://github.com/Nikyle-Naicker/Library_Game",
-                     "N/A")
+                     "N/A",
+                     2)
+            };
+            _images = new List<ProjectImages>
+            {
+                new ProjectImages(
+                    1,
+                    "/pictures/finshark/finsharkhome.png",
+                    "Test1"),
+                new ProjectImages(
+                    1,
+                    "/pictures/finshark/finsharksearch.png",
+                    "Test2"),
+                new ProjectImages(
+                    1,
+                    "/pictures/finshark/finsharkcompanyprofile.png",
+                    ""),
 
-            };    
+                new ProjectImages(
+                    2,
+                    "/pictures/dewey/deweygamesort.png",
+                    "Test3")
+            };
         }
 
         public IEnumerable<Project> GetProjects()
@@ -160,6 +60,11 @@ namespace Portfolio
         public Project GetProjectByTitle(string title)
         {
             return _projects.Where(p => p.Title == title).FirstOrDefault();
+        }
+
+        public IEnumerable<ProjectImages> GetImages(int imageId)
+        {
+            return _images.Where(i => i.ImageId == imageId).ToList();
         }
     }
 }
